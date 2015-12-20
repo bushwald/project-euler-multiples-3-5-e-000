806929bf02b100f53781560473d967cd7cb85e32
Bradley Allen <bradley.allen84@gmail.com>
2015-12-20 20:09:52 +0000

    implement procedural and oop solutions

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,17 @@
 # Enter your procedural solution here!
+def collect_multiples(number)
+	multiples = Array.new
+	for i in 1..number-1
+		multiples << i if i == 3 || i == 5 || i % 3 == 0 || i % 5 == 0
+	end
+	return multiples
+end
+
+def sum_multiples(number)
+	sum = 0
+	multiples = collect_multiples(number)
+	multiples.each {|x|
+		sum = sum + x
+	}
+	return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,29 @@
 # Enter your object-oriented solution here!
+class Multiples
+	def initialize(number)
+		@number = number
+		@multiples = Array.new
+		@sum = 0
+	end
+	
+	def collect_multiples
+		for i in 1..@number-1
+			@multiples << i if i == 3 || i == 5 || i % 3 == 0 || i % 5 == 0
+		end
+		return @multiples
+	end
+
+	def sum_multiples
+		multCollected = false
+		if @multiples.length == 0
+			multCollect = true
+			@sum = 0
+		end
+
+		self.collect_multiples if multCollected == false
+		@multiples.each {|x|
+			@sum = @sum + x
+		}
+		return @sum
+	end
+end	
